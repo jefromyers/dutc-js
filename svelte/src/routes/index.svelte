c65b52d0df53e9a31cdd503234d982a5d162ac95
Jef Myers <jefmyers@gmail.com>
2022-07-30 12:24:39 -0400

    add some sweet websockets to svelte

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -1,1 +1,18 @@
+<svelte:head>
+  <title>Hello</title>
+</svelte:head>
+
+<script>
+import { onMount } from 'svelte';
+
+let ws
+onMount(async () => {
+    ws = new WebSocket('ws://127.0.0.1:8000/ws');
+    ws.onmessage = (msg) => {
+        console.log('Server says: ' + msg)
+        ws.send('Hello there from Svelte')
+    }
+});
+</script>
+
 <h2>Howdy!</h2>
