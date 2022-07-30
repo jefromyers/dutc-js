f8251360ffe1d30fae86d22fb51a27949acde47d
Jef Myers <jefmyers@gmail.com>
2022-07-30 13:54:28 -0400

    create `Campaign` from svelte

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -4,6 +4,7 @@
 
 <script>
 import { onMount } from 'svelte';
+import superagent from 'superagent';
 
 let ws
 onMount(async () => {
@@ -13,6 +14,16 @@ onMount(async () => {
         ws.send('Hello there from Svelte')
     }
 });
+
+let title = 'Campaign'
+function addCampaign(){
+    superagent.post('http://127.0.0.1:8000/v1/campaigns/add')
+                .send(JSON.stringify({title}))
+                .then(res => {console.log(res.body)})
+}
 </script>
 
 <h2>Howdy!</h2>
+
+<input bind:value={title} type="text" />
+<button on:click={addCampaign}>Add Campaign</button>
