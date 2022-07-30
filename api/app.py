9d34dab090fca389e1ae0f0622a8b9d3366c607c
Jef Myers <jefmyers@gmail.com>
2022-07-30 19:56:12 -0400

    clumsy way to talk to everyone

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 import logging
+from dataclasses import dataclass, field
 import sqlalchemy
 from contextlib import contextmanager
 from random import choice
@@ -8,7 +9,8 @@ from starlette.applications import Starlette
 from starlette.routing import Route
 from starlette.responses import JSONResponse
 from starlette.routing import WebSocketRoute
-from starlette.websockets import WebSocket
+from starlette.endpoints import WebSocketEndpoint
+from starlette.websockets import WebSocket, WebSocketState
 from database import SessionLocal, engine, Base, Campaign
 
 Base.metadata.create_all(bind=engine)
@@ -23,12 +25,62 @@ def session():
     finally:
         db.close()
 
-def ping(request):
-    ''' Signs of life '''
+@dataclass
+class SocketPool:
+    pool:set = field(default_factory=set) 
 
+    def add(self, connection):
+        ''' Add a connected WebSocket to the pool '''
+        self.pool.add(connection)
+
+    def remove(self, connection):
+        ''' Remove a connected WebSocket to the pool '''
+        self.pool.remove(connection)
+
+    async def broadcast_all(self, msg):
+        ''' Broadcast to everyone in the pool if they're connected '''
+
+        for c in self.pool:
+            if c.client_state == WebSocketState.CONNECTED:
+                await c.send_text(msg)
+
+    def __len__(self):
+        return len(self.pool)
+
+Pool = SocketPool()
+
+class Socket(WebSocketEndpoint):
+    encoding = "text"
+    
+    async def on_connect(self, websocket):
+        await websocket.accept()
+        Pool.add(websocket)
+        logger.info(f'Added WebSocket connetion to Pool, size is now {len(Pool)}')
+
+    async def on_receive(self, websocket, data):
+        logger.info(f'Recieved: {data}')
+
+    async def on_disconnect(self, websocket, close_code):
+        Pool.remove(websocket)
+        logger.info(f'Removed WebSocket connetion from Pool, size is now {len(Pool)}')
+
+async def ping(request):
+    ''' Signs of life '''
+    
+    # await Pool.broadcast_all("woop")
+    logger.info(f'Connections: {len(Pool)}')
     return JSONResponse(
-        {'ping': 'pong'}, 
-        headers = {"Access-Control-Allow-Origin": request.headers['Origin']}
+        {'ping': 'pong', 'connections': len(Pool)}, 
+        # headers = {"Access-Control-Allow-Origin": request.headers['Origin']}
+    )
+
+async def send_all(request):
+    ''' Signs of life '''
+    
+    await Pool.broadcast_all("woop")
+    return JSONResponse(
+        {'ping': 'pong', 'connections': len(Pool)}, 
+        # headers = {"Access-Control-Allow-Origin": request.headers['Origin']}
     )
 
 async def add_campaign(request):
@@ -55,17 +107,11 @@ async def add_campaign(request):
         headers = {"Access-Control-Allow-Origin": request.headers['Origin']}
     )
 
-async def ws_index(ws):
-    await ws.accept()
-    await ws.send_text('sup')
-    while True:
-        msg = await ws.receive_text()
-        logger.info(f'Message from client: {msg}')
-    await websocket.close()
 
 routes = [
-    WebSocketRoute("/ws", endpoint=ws_index),
+    WebSocketRoute("/ws", Socket),
     Route("/", endpoint=ping, methods=["GET"]),
+    Route("/send", endpoint=send_all, methods=["GET"]),
     Route("/v1/campaigns/add", endpoint=add_campaign, methods=["POST"]),
 ]
 
